33f768a16377867c84f524ac618fdee3722bffc1
X1Warrior <zsto7638@gmail.com>
2017-09-15 11:51:49 -0600

    Made Classes for packages

---
+++ src/monster/controller/MonsterRunner.java
@@ -0,0 +1,12 @@
+package monster.controller;
+
+public class MonsterRunner
+{
+
+	public static void main(String [] args)
+	{
+		MonsterController app = new MonsterController();
+		app.start();
+			
+	}
+}
