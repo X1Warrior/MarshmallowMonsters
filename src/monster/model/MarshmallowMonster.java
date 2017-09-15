703f747c18cced770e5ae94393e79f4cd5b8cbf9
X1Warrior <zsto7638@gmail.com>
2017-09-15 12:00:43 -0600

    Added contstuctors and name = name

--- src/monster/model/MarshmallowMonster.java
+++ src/monster/model/MarshmallowMonster.java
@@ -2,5 +2,27 @@ package monster.model;
 
 public class MarshmallowMonster
 {
-
+	//Declaration section of data members
+	
+	private String name;
+	private int eyeCount;
+	private int armCount;
+	private double tentacleAmount;
+	private boolean hasBloop;
+	
+	
+	public MarshmallowMonster()
+	{
+		//initializes everything to 0, false or null
+	}
+	
+	public MarshmallowMonster(String name, int eyecount, int arms, int tentacleCount, boolean hasbloop)
+	{
+		this.name = name;
+		this.eyeCount = eyeCount;
+		this.armCount = arms;
+		this.tentacleAmount = tentacleAmount;
+		this.hasBloop = hasBloop;
+		
+	}
 }
